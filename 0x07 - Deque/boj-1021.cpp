1357ce4d722713dc4e227a249276fd5f73b9d2df
Kang-Eunsu <kanges802@g.skku.edu>
2024-05-04 22:45:17 +0900

    boj 1021 complete

--- 0x07 - Deque/boj-1021.cpp
+++ 0x07 - Deque/boj-1021.cpp
@@ -9,10 +9,36 @@ int main(void){
     cin >> N;
     cin >> start;
     deque<int> DQ;
+    int num = 0;
 
-    DQ.push_front(1);
-    DQ.push_back(3);
-    int idx = find(DQ.begin(), DQ.end(), 3) - DQ.begin();
+    for (int i = 0; i < N; i++)
+        DQ.push_back(i+1);
+
+    for (int i = 0; i < start; i++){
+        int value;
+        cin >> value;
+        int idx = find(DQ.begin(), DQ.end(), value) - DQ.begin();
+        if (idx < DQ.size() - idx){
+            num += idx;
+            while (idx--) {
+                DQ.push_back(DQ.front());
+                DQ.pop_front();
+            }
+        } else {
+            idx = DQ.size() - idx;
+            num += idx;
+            while (idx--) {
+                DQ.push_front(DQ.back());
+                DQ.pop_back();
+            }
+        }
+        DQ.pop_front();
+
+    }
+    cout << num;
+    
+
+
 
 
 
