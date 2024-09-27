f4a6c2364794ea039d008f6f5840548b73b084f4
Kang-Eunsu <kanges802@g.skku.edu>
2024-09-27 16:18:53 +0900

    boj 11047 and 1931 clear

---
+++ 0x11-그리디/boj-11047.py
@@ -0,0 +1,22 @@
+n, k = list(map(int, input().split()))
+
+coins = []
+index = None
+for i in range(n):
+    coin = int(input())
+    coins.append(coin)
+    if coin <= k:
+        index = i
+
+num = 0
+while k > 0 and index >= 0:
+    coin = coins[index]
+    if coin <= k:
+        need_count = k // coin
+        num += need_count
+        k -= need_count * coin
+
+    index -= 1
+
+
+print(num)
---
+++ 0x11-그리디/boj-1931.py
@@ -0,0 +1,18 @@
+# 15:45~
+n = int(input())
+times = []
+for i in range(n):
+    s, f = list(map(int, input().split()))
+    times.append([s, f])
+
+cur_finish = 0
+num = 0
+times.sort(key=lambda x: (x[1], x[0]))
+
+for time in times:
+    s, f = time
+    if s >= cur_finish:
+        num += 1
+        cur_finish = f
+
+print(num)
