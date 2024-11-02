9f17323a7a5f2b23b19d9885270941b7d3b39dbb
Kang-Eunsu <kanges802@g.skku.edu>
2024-11-02 23:49:58 +0900

    refactor coding test challenge 36 code

--- programmers/lv2/N 진수.py
+++ programmers/lv2/N 진수.py
@@ -1,15 +1,25 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/17687?language=python3
-def convert_n(x, n):
+def convert_n(x, base):
     if x == 0:
         return "0"
-    tmp = ""
-    while x >= 1:
-        rest = x % n
-        rest = f"{rest}" if rest < 10 else chr(ord("A") + (rest - 10))
-        tmp = rest + tmp
-        x = x // n
 
-    return tmp
+    """Claude를 통해 개선한 코드"""
+    digits = "0123456789ABCDEF"
+    result = ""
+
+    while x >= 1:
+        result = digits[x % base] + result
+        x = x // base
+
+    """기존에 작성한 코드"""
+    # tmp = ""
+    # while x >= 1:
+    #     rest = x % n
+    #     rest = f"{rest}" if rest < 10 else chr(ord("A") + (rest - 10))
+    #     tmp = rest + tmp
+    #     x = x // n
+
+    return result
 
 
 def solution(n, t, m, p):
@@ -19,8 +29,7 @@ def solution(n, t, m, p):
         all += convert_n(i, n)
         i += 1
 
-    all = all[: t * m]
-    answer = all[p - 1 :: m]
+    answer = all[p - 1 : t * m : m]
 
     # answer = "".join([all[p - 1 + i * m] for i in range(t)])
     return answer
